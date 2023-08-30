b8c477b7d6b50c41e2c421653cabc2abd540cde0
HUGO25102005 <143435622+HUGO25102005@users.noreply.github.com>
2023-08-30 11:56:25 -0600

    Updated so you can move objects

--- pipegame.py
+++ pipegame.py
@@ -1,27 +1,45 @@
-# Pipes Game (In progress)
+#Short Circuit
+import os,sys
+import pygame as pg #Avoid namespace flooding
 import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+class Character:
+    def __init__(self,rect):
+        self.rect = pg.Rect(rect)
+        self.click = False
+        self.image = pg.Surface(self.rect.size).convert()
+        self.image = pygame.image.load("OneDrive\Escritorio\Videojuego\images/hori_block.png")
+    def update(self,surface):
+        if self.click:
+            self.rect.center = pg.mouse.get_pos()
+        surface.blit(self.image,self.rect)
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def main(Surface,Player):
+    game_event_loop(Player)
+    Surface.fill(0)
+    Player.update(Surface)
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("yellow")
 
-    # RENDER YOUR GAME HERE
+def game_event_loop(Player):
+    for event in pg.event.get():
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if Player.rect.collidepoint(event.pos):
+                Player.click = True
+        elif event.type == pg.MOUSEBUTTONUP:
+            Player.click = False
+        elif event.type == pg.QUIT:
+            pg.quit(); sys.exit()
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+if __name__ == "__main__":
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    pg.init()
+    Screen = pg.display.set_mode((1000,600))
+    MyClock = pg.time.Clock()
+    MyPlayer = Character((0,0,100,100))
+    MyPlayer.rect.center = Screen.get_rect().center
+    while 1:
+        main(Screen,MyPlayer)
+        pg.display.update()
+        MyClock.tick(60)
 
-    clock.tick(60)  # limits FPS to 60
 
-pygame.quit()
